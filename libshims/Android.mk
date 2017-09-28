b6b09a7a97755febe872a528339b8c81649216be
Shankar D. Warang <shankar.warang9@gmail.com>
2017-09-28 23:54:17 +0530

    Add files via upload

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -16,8 +16,9 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := libshim_ril.cpp libshim_wpa_supplicant.cpp libshim_gui.cpp libshim_wvm.cpp libshim_gps.cpp
-LOCAL_SHARED_LIBRARIES := libbinder libgui libutils liblog libbinder libandroid libui libmedia libstagefright_foundation libicuuc
+LOCAL_SRC_FILES := libshim_ril.cpp libshim_wpa_supplicant.cpp libshim_gui.cpp libshim_wvm.cpp libshim_wvm2.cpp libshim_gps.cpp
+LOCAL_SHARED_LIBRARIES := libbinder libgui libutils liblog libandroid libui libmedia libstagefright_foundation libicuuc
 LOCAL_MODULE := libshims
+LOCAL_32_BIT_ONLY := true
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
